6e8caff50f34a2667a27abf5a11b0ec99dc76a61
Marshay <marshay.m.zawadi@gmail.com>
2025-03-01 10:24:55 -0500

    refactor: Implement procedural score updates with async/await

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,87 +11,9 @@ import { ScoreboardComponent } from './scoreboard/scoreboard.component';
 })
 export class AppComponent implements OnInit {
   title = 'Hackathon ScoreBoard';
-  teams: ScoreBoardItem[] = [{
-    teamName: "Code Ninjas",
-    teamMembers: ["Me", "MySelf"],
-    codeWarsUser: "mzawam",
-    completedKatas: [],
-    points: 0
-  },{
-    teamName: "Code Ninjas2",
-    teamMembers: ["The Dude", "Donnie"],
-    codeWarsUser: "xDranik",
-    completedKatas: [],
-    points: 0
-  },
-  {
-    teamName: "Code Ninjas3",
-    teamMembers: ["Josh", "Noah"],
-    codeWarsUser: "Cousin-Joe",
-    completedKatas: [],
-    points: 0
-  },
-  {
-    teamName: "Code Ninjas4",
-    teamMembers: ["Ian", "John"],
-    codeWarsUser: "cyclump",
-    completedKatas: [],
-    points: 0
-  },
-  {
-    teamName: "Code Ninjas5",
-    teamMembers: ["Joseph", "Donnie"],
-    codeWarsUser: "10PercentTestCoverage",
-    completedKatas: [],
-    points: 0
-  },
-  {
-    teamName: "Code Ninjas6",
-    teamMembers: ["Steve", "Morty"],
-    codeWarsUser: "SteveSitko",
-    completedKatas: [],
-    points: 0
-  },{
-    teamName: "Code Ninjas",
-    teamMembers: ["Me", "MySelf"],
-    codeWarsUser: "mzawam",
-    completedKatas: [],
-    points: 0
-  }, {
-    teamName: "Code Ninjas2",
-    teamMembers: ["The Dude", "Donnie"],
-    codeWarsUser: "xDranik",
-    completedKatas: [],
-    points: 0
-  },
-  {
-    teamName: "Code Ninjas3",
-    teamMembers: ["Josh", "Noah"],
-    codeWarsUser: "Cousin-Joe",
-    completedKatas: [],
-    points: 0
-  },
-  {
-    teamName: "Code Ninjas4",
-    teamMembers: ["Ian", "John"],
-    codeWarsUser: "cyclump",
-    completedKatas: [],
-    points: 0
-  },
-  {
-    teamName: "Code Ninjas5",
-    teamMembers: ["Joseph", "Donnie"],
-    codeWarsUser: "10PercentTestCoverage",
-    completedKatas: [],
-    points: 0
-  },
-  {
-    teamName: "Code Ninjas6",
-    teamMembers: ["Steve", "Morty"],
-    codeWarsUser: "SteveSitko",
-    completedKatas: [],
-    points: 0
-  }]; // ... add other teams
+  teams: ScoreBoardItem[] = INITIAL_TEAMS;
+  private readonly POLL_INTERVAL = 300000; // 5 minutes
+  private readonly START_DATE = new Date("2020-04-10");
 
   constructor(private userService: UserService) {}
 
@@ -100,55 +22,96 @@ export class AppComponent implements OnInit {
   }
 
   private startPolling() {
-    // Initial load
     this.updateScores();
-    
-    // Poll every 5 minutes
-    setInterval(() => {
-      this.updateScores();
-    }, 300000);
+    setInterval(() => this.updateScores(), this.POLL_INTERVAL);
   }
 
   private async updateScores() {
     try {
-      for (const team of this.teams) {
-        const challenges = await this.loadTeamChallenges(team);
-        team.completedKatas = [];
-        team.points = 0;
-
-        for (const challenge of challenges) {
-          const details = await this.loadChallengeDetails(challenge.id)
-          if(details){
-            team.completedKatas.push(details.name);
-            team.points += details.rank.id * -1;
-          }
-        }
-      }
+      await Promise.all(
+        this.teams.map(team => this.updateTeamScore(team))
+      );
     } catch (error) {
       console.error('Error updating scores:', error);
     }
   }
 
-  private async loadTeamChallenges(team: ScoreBoardItem) {
-    const response = await this.userService
-      .getCodeChallengesByUser(team.codeWarsUser, 0)
-      .toPromise();
-      if(response){
-        return response.data.filter(challenge => 
-          new Date(challenge.completedAt) > new Date("2024-04-10")
-        );
-      }
-      return []
+  private async updateTeamScore(team: ScoreBoardItem) {
+    const challenges = await this.loadTeamChallenges(team);
+    const details = await this.loadChallengeDetails(challenges);
+    
+    team.completedKatas = details.map(d => d?.name).filter(name => name !== undefined) as string[];
+    team.points = this.calculatePoints(details);
   }
 
-  private async loadChallengeDetails(challengeId: string): Promise<any> {
-    try{
-      return await this.userService
-      .getCodeChallenge(challengeId)
+  private async loadTeamChallenges(team: ScoreBoardItem) {
+    const response = await this.userService 
+      .getCodeChallengesByUser(team.codeWarsUser, 0)
       .toPromise();
-    }catch(error){
-      console.error('Error loading challenge details:', error);
-      return undefined;
-    }
+    
+    if(!response) return [];
+
+    return response.data?.filter(challenge => 
+      new Date(challenge.completedAt) > this.START_DATE
+    );
+  }
+
+  private async loadChallengeDetails(challenges: any[]) {
+    return await Promise.all(
+      challenges.map(challenge => 
+        this.userService.getCodeChallenge(challenge.id).toPromise()
+      )
+    );
+  }
+
+  private calculatePoints(challenges: any[]): number {
+    return challenges.reduce((total, challenge) => 
+      total + (challenge.rank.id * -1), 0
+    );
   }
 }
+
+const INITIAL_TEAMS: ScoreBoardItem[] = [
+  {
+    teamName: "Code Ninjas",
+    teamMembers: ["Me", "MySelf"],
+    codeWarsUser: "mzawam",
+    completedKatas: [],
+    points: 0
+  },
+  {
+    teamName: "Code Ninjas2",
+    teamMembers: ["The Dude", "Donnie"],
+    codeWarsUser: "xDranik",
+    completedKatas: [],
+    points: 0
+  },
+  {
+    teamName: "Code Ninjas3",
+    teamMembers: ["Josh", "Noah"],
+    codeWarsUser: "Cousin-Joe",
+    completedKatas: [],
+    points: 0
+  },
+  {
+    teamName: "Code Ninjas4",
+    teamMembers: ["Ian", "John"],
+    codeWarsUser: "cyclump",
+    completedKatas: [],
+    points: 0
+  },
+  {
+    teamName: "Code Ninjas5",
+    teamMembers: ["Joseph", "Donnie"],
+    codeWarsUser: "10PercentTestCoverage",
+    completedKatas: [],
+    points: 0
+  },
+  {
+    teamName: "Code Ninjas6",
+    teamMembers: ["Steve", "Morty"],
+    codeWarsUser: "SteveSitko",
+    completedKatas: [],
+    points: 0
+  }
+];
